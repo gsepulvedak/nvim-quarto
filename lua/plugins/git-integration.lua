4bd0538eeff506187fb89354963fd44c9bb9afe1
pj <pete@petejon.es>
2024-11-19 16:06:28 -0300

    add lazygit integration

--- lua/plugins/git-integration.lua
+++ lua/plugins/git-integration.lua
@@ -14,5 +14,29 @@ return {
   { -- Adds Tim Pope's fugitive plugin which serves as a wrapper for CLI git
     'tpope/vim-fugitive',
   },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { 
+        "<leader>lg", 
+        "<cmd>LazyGit<cr>", 
+        desc = "Open LazyGit in floating buffer"
+      },
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
