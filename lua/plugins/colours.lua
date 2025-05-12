886de590ce471056e718c3dcbdd94f690ce9b986
pj <pete@petejon.es>
2025-05-12 15:19:47 -0300

    small colour scheme and font changes

--- lua/plugins/colours.lua
+++ lua/plugins/colours.lua
@@ -1,11 +1,16 @@
 return {
   {
     'catppuccin/nvim',
+    enabled = true,
     lazy = false,
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      -- Set the colour scheme installed via lua/lazy-plugins.lua
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
+    end,
+    init = function()
       vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
