0ba3c6fe7ddaa755b17406d5edb107e72199db62
pj <pete@petejon.es>
2025-05-12 15:00:47 -0300

    update which-key to its v3 spec

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,23 +1,36 @@
 return {
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>D'] = { name = '[D]iagnostics', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>W'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>i'] = { name = '[I]ncremental selection', _ = 'which_key_ignore' },
-        ['<leader>S'] = { name = '[S]lime/[S]end', _ = 'which_key_ignore' },
-        ['<leader>m'] = { name = '[M]arkdown', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]un code', _ = 'which_key_ignore' },
-      }
-    end,
+    event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
+    opts = {
+      spec = {
+        { "<leader>D", group = "[D]iagnostics" },
+        { "<leader>D_", hidden = true },
+        { "<leader>W", group = "[W]orkspace" },
+        { "<leader>W_", hidden = true },
+        { "<leader>c", group = "[C]ode" },
+        { "<leader>c_", hidden = true },
+        { "<leader>d", group = "[D]ocument" },
+        { "<leader>d_", hidden = true },
+        { "<leader>i", group = "[I]ncremental selection" },
+        { "<leader>i_", hidden = true },
+        { "<leader>m", group = "[M]arkdown" },
+        { "<leader>m_", hidden = true },
+        { "<leader>r", group = "[R]un code" },
+        { "<leader>r_", hidden = true },
+        { "<leader>s", group = "[S]earch" },
+        { "<leader>s_", hidden = true },
+      },
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
